ad61aa381c7812e8c16279977083ff110b71de5f
Anthony Fasano <awfasano@gmail.com>
2023-03-14 11:23:43 -0700

    Initial Commit

---
+++ StudyAI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyAI
+//
+//  Created by Anthony Fasano on 3/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
